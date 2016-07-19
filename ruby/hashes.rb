dc6b340f2b9cbb9eb718970149cc9b09daa67850
Kimberly Pena <Kimberly.Pena32@gmail.com>
2016-07-19 12:59:54 -0700

    add pseudocode

---
+++ ruby/hashes.rb
@@ -0,0 +1,7 @@
+#pseudocode:
+#ask user for information: client name, age, number of children, decor theme, city, fav color, on a budget
+#use conversion to make sure that user input is in the appropiate data type; age -> integer, city ->string, on a budget -> true/false
+#ask user if they would like to make any changes to their input
+#create branches to accommodate any updates using if statements and then update any given variable
+#open a new hash name:interior design client
+#add each key and value to the hash
