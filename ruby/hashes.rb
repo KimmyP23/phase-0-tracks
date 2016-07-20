c906e75bd3878b88df4626acd11183d8b5784092
Kimberly Pena <Kimberly.Pena32@gmail.com>
2016-07-20 14:04:47 -0700

    create method to display possible updates

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -34,52 +34,54 @@ interior_design_client[:number_of_children] = number_of_children
 interior_design_client[:decor_theme] = decor_theme
 interior_design_client[:city] = city
 interior_design_client[:on_budget] = on_budget
-p interior_design_client
+puts interior_design_client
 
-
-puts "Would you like to make any updates?(yes/no)"
-updates = gets.chomp.downcase
-
-if updates == "yes"
-	puts "What would you like to update?
-		client name
-		age
-		number of children
-		decor theme
-		city
-		budget"
-	new_update = gets.chomp.downcase
-	if new_update == "client name"
-		puts "Enter new client name:"
-		new_client_name =gets.chomp
-		client_name = new_client_name
-	elsif new_update == "age"
-		puts "Enter new age:"
-		new_age = gets.chomp.to_i
-		age = new_age
-	elsif new_update == "number of children"
-		puts "Enter new number of children:"
-		new_number_of_children = gets.chomp.to_i
-		number_of_children = new_number_of_children
-	elsif new_update == "decor theme"
-		puts "Enter new decor theme:"
-		new_decor_theme = gets.chomp
-		decor_theme = new_decor_theme
-	elsif new_update == "city"
-		puts "Enter new city:"
-		new_city = gets.chomp
-		city = new_city
-	else
-		puts "Is client on a buget?(yes/no)"
-		new_budget = gets.chomp.downcase
-			if new_budget == "yes"
-				budget = true
-			else
-				budget = false
-			end
-		on_budget = new_budget
-	end	
+def display_keys(hash)
+	hash.each {|key, value| puts "#{key}"}
 end
+
+while true
+	puts "Would you like to make any updates?(yes/no)"
+	updates = gets.chomp.downcase
+	
+	break if updates != "yes"
+
+	puts "What would you like to update?"
+	display_keys(interior_design_client)
+	puts "Enter your selection:"
+	new_update = gets.chomp.downcase
+
+		if new_update == "client name"
+			puts "Enter new client name:"
+			new_client_name =gets.chomp
+			client_name = new_client_name
+		elsif new_update == "age"
+			puts "Enter new age:"
+			new_age = gets.chomp.to_i
+			age = new_age
+		elsif new_update == "number of children"
+			puts "Enter new number of children:"
+			new_number_of_children = gets.chomp.to_i
+			number_of_children = new_number_of_children
+		elsif new_update == "decor theme"
+			puts "Enter new decor theme:"
+			new_decor_theme = gets.chomp
+			decor_theme = new_decor_theme
+		elsif new_update == "city"
+			puts "Enter new city:"
+			new_city = gets.chomp
+			city = new_city
+		else
+			puts "Is client on a buget?(yes/no)"
+			new_budget = gets.chomp.downcase
+				if new_budget == "yes"
+					budget = true
+				else
+					budget = false
+				end
+			on_budget = new_budget
+		end	
+	end
 
 interior_design_client[:client_name] = client_name
 interior_design_client[:age] = age
