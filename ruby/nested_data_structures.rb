e7b3525959c6c7a7923f2e1b889f0da02fe7adf3
Kimberly Pena <Kimberly.Pena32@gmail.com>
2016-07-21 19:37:07 -0700

    add hash with keys to structure

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,7 @@
+stadium = {
+	'Stands' => ["outfield bleachers", "behind home plate premium seats", "VIP lounge", "left-field seating", "right-field seating"]
+	'Field' =>
+	'food vendors' =>
+	'Press box' =>
+
+}
