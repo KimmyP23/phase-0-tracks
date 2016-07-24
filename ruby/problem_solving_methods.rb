35199f37e92be2cb0f551f800cff2a13a9ad9f54
Kimberly Pena <Kimberly.Pena32@gmail.com>
2016-07-23 21:52:18 -0700

    Add fibonacci method

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -14,3 +14,27 @@ arr = [42, 89, 23, 1]
   
 p search_array(arr, 1)
 p search_array(arr, 90)
+
+# Release 5.6.1
+
+#create Fibonacci method
+def fib(num)
+#Created empty array to pass Fib sequence into
+  fib_array = []
+#Counted from 0 to inputed number to create iteration
+  0.upto (num - 1) do |n|
+#If n is less than 2 (0,1), add n to array 
+    if n < 2
+      fib_array << n
+#If n is greater than 2, add the previous two elements of the array to get next value and add to the sequence in array
+    else
+    fib_array << (fib_array[-1] + fib_array[-2])
+    end
+  end
+    fib_array
+end
+
+puts fib(6)
+
+#use fib_array.last when you just want to check the last element in the array
+# p fib(100) == 218922995834555169026
