6171d4bb89780b04d2fb68bea8ba7dfccf14674c
Kimberly Pena <Kimberly.Pena32@gmail.com>
2016-07-24 15:55:48 -0700

    Add user interface and stored data in array

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -55,7 +55,28 @@ def alias_creator(first_name, last_name)
  	spy_alias = last_name_array.join('').capitalize + " " + first_name_array.join('').capitalize
  end
 
-
+#create empty array to hold spy aliases
+spy_aliases = []
+first_name = ''
+last_name = ''
+#initialize loop
+while first_name != "quit" || last_name != "quit"
+#ask for first name
+  	puts "Please enter your first name to get alias: (or Type 'quit' to stop)"
+  	first_name = gets.chomp.downcase
+ #ask for last name
+  	puts "Please enter your last name to get alias:(or type 'quit' to stop)"
+  	last_name = gets.chomp.downcase
+  	full_name = first_name.capitalize + ' ' + last_name.capitalize
+  
+  	puts "Your new alias is #{alias_creator(first_name, last_name)}"
+ #add aliases to spy_aliases array
+  	spy_aliases << [full_name, alias_creator(first_name, last_name)]
+end
+ 
+p spy_aliases
+spy_aliases.each {|name| puts "#{name[1]} is actually #{name[0]}"}
+ 
 
 
 
