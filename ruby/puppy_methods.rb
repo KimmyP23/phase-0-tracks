5281c9b3062a924ac192373e7fe4f96f893c8e0e
Kimberly Pena <Kimberly.Pena32@gmail.com>
2016-07-26 16:31:04 -0700

    add class methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,37 @@
+#Release 6.2
+#Pairing: Kimberly Pena and Thomas Logan Graves
+
 class Puppy
 
   def fetch(toy)
     puts "I brought back the #{toy}!"
     toy
   end
-
+  
+  def speak(i)
+      i.times { puts "Woof!" }
+  end
+  
+  def roll_over
+      puts "*Rolls Over*"
+  end
+  
+  def dog_years(i)
+      puts "#{i * 7}"
+  end
+  
+  def shake
+      puts "*Holds up paw*"
+  end
+  
 end
+
+puppy = Puppy.new
+
+puppy.fetch("ball")
+puppy.speak(3)
+puppy.roll_over
+puppy.dog_years(4)
+puppy.shake
+
 
