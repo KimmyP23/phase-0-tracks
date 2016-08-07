92a90005630a1105c12bc48798485203bf0dd4c2
Kimberly Pena <Kimberly.Pena32@gmail.com>
2016-08-07 10:31:15 -0700

    add release 1:create object

--- js/data_structures.js
+++ js/data_structures.js
@@ -8,3 +8,11 @@ horses.push("American Pharoah");
 console.log(colors);
 console.log(horses);
 
+// Release 1: Create Object
+var horseObject = {};
+
+for (var i = 0; i < colors.length; i++) {
+  horseObject[horses[i]] = colors[i];
+}
+
+console.log(horseObject);
