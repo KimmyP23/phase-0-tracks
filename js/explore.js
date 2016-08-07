b767424e4170e1240253966a4568060092e9b182
Kimberly Pena <Kimberly.Pena32@gmail.com>
2016-08-07 10:20:12 -0700

    Add explore file and add pseudocode

---
+++ js/explore.js
@@ -0,0 +1,5 @@
+//pseudocode for reverse method
+//1. create a function that takes in a string
+//2. split the string into an array
+//3. with a for loop, add the indicies of the array, starting from the end 1 by 1 into a new/blank string
+//4. return the new string
