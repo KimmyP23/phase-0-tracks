5512122accbfe69be5957bddc4c876c1fb476ffc
Kimberly Pena <Kimberly.Pena32@gmail.com>
2016-08-07 21:28:22 -0700

    add hash matcher function and driver code

--- js/algos.js
+++ js/algos.js
@@ -33,3 +33,32 @@ var array2 = ["this", "is", "complete", "array"]
 console.log("array2's longest word is: " + findLongestWord(array2));
 var array3 = ["making", "new", "arrays", "of", "words", "with", "ruby", "%w()", "shenanigans"]
 console.log("array3's longest word is: " + findLongestWord(array3));
+
+// Release 1
+
+// Start release 1 Pseudocode
+    // create a function that takes 2 variables, name and age
+    // test conditionally
+    // if input_1 :name or age matches the input_2 :name or age
+    // return true if any match between input_1 and input_2 exist
+    // name contains or .include?(input2 variable)
+    // else return false
+// End release 1 Pseudocode
+
+function hashMatcher(personHash1, personHash2)
+{
+  if ((personHash1.name == personHash2.name) ||
+   (personHash1.age == personHash2.age))
+  {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// Begin release 1 driver code
+
+var personHash1 = {name: "James", age: 39};
+var personHash2 = {name: "James", age: 32};
+
+console.log(hashMatcher(personHash1, personHash2));
