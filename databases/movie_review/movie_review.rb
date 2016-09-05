f4f7365264d29dbed9c24e574b439a6680abe104
Kimberly Pena <Kimberly.Pena32@gmail.com>
2016-09-05 07:23:45 -0700

    add ruby interface to get review from user

--- databases/movie_review/movie_review.rb
+++ databases/movie_review/movie_review.rb
@@ -22,3 +22,46 @@ def new_review(db, name, rating, recommended, comments)
 end
 
 #USER INTERFACE
+# Ask user about new movie to review
+puts "Welcome to Movie Reviewer - where the only movie critic's opinion that matters is YOURS!"
+new_movie = ''
+until new_movie == 'no'
+  puts "\nWould you like to add a new movie review? (yes/no)"
+  new_movie = gets.chomp
+
+  puts "What movie did you see?"
+  new_movie = gets.chomp
+
+  puts "On a scale from 1-10, what would you rate the movie?"
+  user_rating = gets.chomp.to_i
+
+    until user_rating > 0 && user_rating <= 5
+      puts "Oh boy, your rating is off the charts! No, but seriously, it's off the chart. Please pick a number between 1-5"
+      user_rating = gets.chomp.to_i
+    end
+
+  puts "Would you recommend the movie to a friend? (yes/no)"
+  user_recommendation = false
+  until user_recommendation
+    recommendation_response = gets.chomp
+    if recommendation_response.downcase == "yes"
+      user_recommendation = "true"
+      true
+    elsif recommendation_response.downcase == "no"
+      user_recommendation = "false"
+      true
+    else
+      puts "I'm sorry, I didn't understand your response, please answer 'yes' or 'no'"
+    end
+  end
+
+  puts "Any additional thoughts/comments about the movie?"
+  comments_response = gets.chomp
+  if comments_response.downcase == "no" || comments_response == ""
+    user_comments = "N/A"
+  else user_comments = comments_response
+  end
+
+  # Add new review to movies database
+  new_review(db, new_movie, user_rating, user_recommendation, user_comments)
+end
